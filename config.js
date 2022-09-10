0dab193407d3d85652e6372a74fe3222e33c2dc8
surajit123323 <111307954+surajit123323@users.noreply.github.com>
2022-09-11 02:19:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -42,11 +42,11 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['918270026275','916380260672'] //you can add multiple number
-global.ownername = "𝗧𝚯𝗫𝗜𝗖 𝗧𝗨𝗥𝗕𝚯"
+global.owner = ['917605893554','919836865713'] //you can add multiple number
+global.ownername = "𝗧𝚯𝗫𝗜𝗖 SURYA"
 global.ytname = "YT: Turbo Mods" //ur yt chanel name
 global.socialm = "GitHub: TURBOHYPER" //ur github or insta name
-global.location = "India, TamilNadu, Coimbatore" //ur location
+global.location = "India, WestBengal, Kolkata" //ur location
 
 //bot bombdy
 global.botname = "𝗧𝚯𝗫𝗜𝗖 ₪ 𝚫𝗟𝗘𝗫𝚫" //ur bot name
@@ -56,8 +56,8 @@ global.packname = "𝗧𝚯𝗫𝗜𝗖 ₪ 𝚫𝗟𝗘𝗫𝚫"
 global.author = "𝗧𝚯𝗫𝗜𝗖 𝗧𝗨𝗥𝗕𝚯"
 global.themeemoji = "💖"
 global.reactmoji = "🌹"
-global.ownertag = ['916380260672'] //ur owner number
-global.ownernummenu = ['916380260672'] //ur owner number in menu and all
+global.ownertag = ['917605893554'] //ur owner number
+global.ownernummenu = ['917605893554'] //ur owner number in menu and all
 global.watermark = " 𝗧𝚯𝗫𝗜𝗖 𝗧𝗨𝗥𝗕𝚯" //ur watermark
 global.botscript = "https://github.com/TURBOHYPER/Toxic-Alexa_V3"
 global.linkz1 = "https://chat.whatsapp.com/LWjJ4tu2qe9BWQZ1JzRZgp"
